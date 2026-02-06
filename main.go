629f0961a0d94f0b936ff68185fc145651c021f1
gui castaldeli <guicastaldeli8@gmail.com>
2026-02-06 16:35:14 -0400

    bat update

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func test() string {
+	return "testtttt go"
+}
+
 func main() {
-	println("hello!!!")
+	var test string = test()
+	var test2 string
+	test2 = "test2!!"
+	println(test, test2)
 	fmt.Println("hello!!fmt")
 }
